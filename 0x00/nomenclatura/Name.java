272d5dfc22e3b71b187b87c7c0d9600eb77e1f66
Maikon Jakson <107194793+MjaksonCiandT@users.noreply.github.com>
2022-07-18 14:02:51 -0300

    Update Name.java

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -1,48 +1,19 @@
-import java.util.Date;
-
 public class Name {
+
     public Date modifiedDate;
     public List<Testcase> findAllTestcasesByUser(Name.User user){};
 
-    public Date d;
-    public List<Testcase> find(Name.User user){};
-
     public List<Account> AccountList;
-
-    public Account[] AccountList;
-
-
-
-    public void deleteUser(Name.User pUser){};
-
+    
     public void deleteUser(Name.User user){};
 
-
-    public URL r;
-
     public URL url;
-
 
     public class TableRepresent {};
     public void saveCar(){};
 
-    public class TableRepresenter{};
-    public void car(){};
+    public saveUser(){}; saveAccount(){}; saveContract(){};
 
-
-    public saveUser(){
-    }; saveAccount(); saveContract(){};
-
-    
-    createAccount(){
-    };
-    
-    generateContract(){
-    };
-
-
-
-    public int daysInYear = 360;
-    public int hoursInWeek = 168;
-
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
 }
